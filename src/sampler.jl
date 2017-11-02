1ee4c1775223953a52c41d6edb824e7aeefad734
Roger Heristad <roger.herikstad@gmail.com>
2017-11-02 17:27:25 +0800

    Fixes an issue with the data sampler

--- src/sampler.jl
+++ src/sampler.jl
@@ -1,24 +1,28 @@
 function get_continuous_data(socket::UDPSocket, time_window::Float64)
     t0 = 0.0
+    t1 = 0.0
     data = Array{Int16}(0) 
-    while t0 < time_window
+    source_module = Array{UInt8}(0)
+    while (t1-t0) < time_window
         bytes = recv(socket)
         n = length(bytes)
         i = 1
         while i <= n
-            packet = XippPacket(bytes[i:end])
+            packet = XippPacket(view(bytes,i:n))
             if (packet.header.processor == 1) && (packet.header._module != 0)
                 if packet.header.stream != 0
                     data_packet = XippDataPacket(packet)
                     if data_packet.stream_type == XIPP_STREAM_CONTINUOUS
                         c_data_packet = XippContinuousDataPacket(data_packet)
-                        t0 += length(c_data_packet.i16)/30.0 #convert to ms
+			t0 = t1
+                        t1 = float(packet.header._time)
                         append!(data, c_data_packet.i16)
+			append!(source_module, c_data_packet.header_module)
                     end
                 end
             end
             i += size(packet)
         end
     end
-    data
+    data, source_module
 end
