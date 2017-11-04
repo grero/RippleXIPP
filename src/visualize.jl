9381da4fea09f1ba657cb4cb54b30831926e527b
roger <roger.herikstad@gmail.com>
2017-11-04 10:41:39 +0800

    Adds function for visualizing continuous data as a rotating buffer

--- src/visualize.jl
+++ src/visualize.jl
@@ -29,3 +29,42 @@ function show_data(socket, channel=1)
       _view(visualize(data, :lines, color=acolor), window)
       renderloop(window)
 end
+
+function show_data(ss::Signal{RippleXIPP.XippContinuousDataPacket}, channel=1, ymin=-10_00, ymax=10_000)
+    window = glscreen()
+    res = widths(window)
+    w,h = res
+    xmargin = 20
+    ymargin = 20
+    Δx = (w-2*xmargin)/30_000
+    Δy = (h-2*ymargin)/(ymax-ymin)
+    dt = 30
+    data = [Point2f0(xmargin + i*Δx, 0.0f0) for i in 1:30_000]
+    const tm = typemax(UInt32)
+    t0 = tm
+    tp = 1
+    tick = bounce(1:10, 100)
+    new_data = map(ss) do _ss
+        t1 = _ss.header._time
+        if t0 == tm
+            t0 = t1
+        end
+        tt = mod(div(t1 - t0,500) + 1, 30_000)+1
+        _dd = _ss.i16[channel]
+        for k in tp:tt
+            data[k] = Point2f0(xmargin + k*Δx, ymargin + (Float32(_dd) - ymin)*Δy)
+        end
+        tp = tt
+        data
+    end
+    plot_data = sampleon(tick, new_data)
+    _view(visualize(plot_data, :lines, color=RGBA(1.0, 0.0, 0.0, 1.0)), window)
+    renderloop(window)
+end
+
+function test()
+    socket = RippleXIPP.create_receiving_socket(ip"127.0.0.1", 2003)
+    packet = zero(RippleXIPP.XippContinuousDataPacket)
+    ss = Signal(packet)
+    ss, socket
+end
