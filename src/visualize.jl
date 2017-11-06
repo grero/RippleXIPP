99552095c65327c256410b95ab495eee75c24fb5
roger <roger.herikstad@gmail.com>
2017-11-06 09:47:35 +0800

    WIP: Tinkering to try and make visualize more consistent

--- src/visualize.jl
+++ src/visualize.jl
@@ -30,39 +30,64 @@ function show_data(socket, channel=1)
       renderloop(window)
 end
 
-function show_data(ss::Signal{RippleXIPP.XippContinuousDataPacket}, channel=1, ymin=-10_00, ymax=10_000)
+function show_data(ss::Signal{RippleXIPP.XippContinuousDataPacket}, channel=1, ymin=-10_000, ymax=10_000)
+    nn = 10000
     window = glscreen()
     res = widths(window)
     w,h = res
     xmargin = 20
     ymargin = 20
-    Δx = (w-2*xmargin)/1e6
+    tmax = 7.8e6 
+    Δx = (w-2*xmargin)/tmax
     Δy = (h-2*ymargin)/(ymax-ymin)
     _scale = scalematrix(Vec3f0(Δx, Δy, 1.0))
     _offset = translationmatrix(Vec3f0(0.0, ymin, 0))
     _margins = translationmatrix(Vec3f0(xmargin, ymargin, 0))
-    #_model = _offset*_scale*_margins
     _model = _margins*_scale*_offset
     dt = 30
-    data = [Point2f0(xmargin + i*Δx, 0.0f0) for i in 1:30_000]
+    data = [Point2f0(xmargin + i*Δx, 0.0f0) for i in 1:nn]
     const tm = typemax(UInt32)
     t0 = tm
     tp = 1
+    t1 = 0 
     tick = bounce(1:10, 100)
     k = 1
+    kk = 1
+    tt = UInt32(0)
+    Δt = 0.0
+    passed_once = false
     new_data = map(ss) do _ss
+        Δt = _ss.header._time  - t1
         t1 = _ss.header._time
         if t0 == tm
             t0 = t1
         end
         tt = t1 - t0
+        #we have already been through one round
+        if tt > tmax
+            passed_once = true
+        end
+        tt = mod(tt, tmax)
+        if passed_once
+            kk = 1
+            while (kk < nn) && (tt > data[kk][1])
+                kk += 1
+            end
+            tt = data[kk][1]
+        else
+            kk = k
+        end
         _dd = _ss.i16[channel]
-        data[k] = Point2f0(tt, _dd)
+        data[kk] = Point2f0(tt, _dd)
         tp = tt
-        k = mod(k, 30_000)+1
+        k = mod(k, nn)+1
         data
     end
     plot_data = sampleon(tick, new_data)
+    #tick2 = bounce(1:10, 1)
+    #preserve(map(tick2) do _tt
+    #             print("$kk ")
+    #         end)
     _view(visualize(plot_data, :lines, color=RGBA(1.0, 0.0, 0.0, 1.0),model=_model), window)
     renderloop(window)
 end
