3697578e2f4ced6e25bc495de9332dfa62caf8b2
roger <roger.herikstad@gmail.com>
2017-11-06 09:47:01 +0800

    Adds code to sample using Reactive

--- src/sampler.jl
+++ src/sampler.jl
@@ -1,9 +1,31 @@
+using DSP
+using Reactive
+
 function get_continuous_data(socket::UDPSocket, time_window::Float64)
     t0 = -1.0
     t1 = -1.0
     data = Array{Int16}(0) 
     source_module = Array{UInt8}(0)
+    ss = Signal(zero(XippContinuousDataPacket))
     while (t1-t0) < time_window
+#        
+#
+#                        if t0 < 0
+#                            t0 = float(packet.header._time)
+#                        end
+#                        t1 = float(packet.header._time)
+#                        append!(data, c_data_packet.i16)
+#                        append!(source_module, c_data_packet.header._module)
+#                    end
+#                end
+#            end
+#        end
+    end
+    data, source_module
+end
+
+function get_continuous_data_packet!(ss::Signal{XippContinuousDataPacket}, socket::UDPSocket)
+    while true
         bytes = recv(socket)
         n = length(bytes)
         i = 1
@@ -13,18 +35,11 @@ function get_continuous_data(socket::UDPSocket, time_window::Float64)
                 if packet.header.stream != 0
                     data_packet = XippDataPacket(packet)
                     if data_packet.stream_type == XIPP_STREAM_CONTINUOUS
-                        c_data_packet = XippContinuousDataPacket(data_packet)
-                        if t0 < 0
-                            t0 = float(packet.header._time)
-                        end
-                        t1 = float(packet.header._time)
-                        append!(data, c_data_packet.i16)
-                        append!(source_module, c_data_packet.header._module)
+                        push!(ss, XippContinuousDataPacket(data_packet))
                     end
                 end
             end
             i += size(packet)
         end
     end
-    data, source_module
 end
